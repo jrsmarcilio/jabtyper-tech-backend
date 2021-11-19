49f350728ae09a5cadecb6ecdae901e304764780
jrsmarcilio <jrsmarcilio@gmail.com>
2021-11-19 15:36:12 -0300

    add enable access control allow origin

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+
 import { router } from "./routes";
 import "./database";
 
@@ -11,6 +12,20 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  next();
+});
+
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof Error)
@@ -23,3 +38,6 @@ app.use(
 );
 
 app.listen(process.env.PORT || 3000, () => console.log("🏃 Running Server"));
+function cors(arg0: { origin: string }): any {
+  throw new Error("Function not implemented.");
+}
