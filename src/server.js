ba20521c7f49b37a149dee1140e2a360c5d14982
jrsmarcilio <jrsmarcilio@gmail.com>
2021-11-19 20:03:47 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -4,13 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
-require("reflect-metadata");
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
+const cors_1 = __importDefault(require("cors"));
+require("reflect-metadata");
 const routes_1 = require("./routes");
 require("./database");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use(cors_1.default);
+app.use((0, cors_1.default)({ origin: "*" }));
 app.use(express_1.default.json());
 app.use(routes_1.router);
 app.use((error, request, response, next) => {
@@ -20,4 +23,4 @@ app.use((error, request, response, next) => {
         .status(400)
         .json({ error: "error", message: "Internal server error" });
 });
-app.listen(process.env.PORT || 3000, () => console.log("🏃 Running Server"));
+app.listen(process.env.PORT || 5000, () => console.log(`🏃 Running Server as ${process.env.PORT || 5000}`));
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
-import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import cors from "cors";
+import "reflect-metadata";
 
 import { router } from "./routes";
 import "./database";
@@ -10,21 +11,10 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors);
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(router);
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  next();
-});
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
@@ -37,7 +27,6 @@ app.use(
   }
 );
 
-app.listen(process.env.PORT || 3000, () => console.log("🏃 Running Server"));
-function cors(arg0: { origin: string }): any {
-  throw new Error("Function not implemented.");
-}
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`🏃 Running Server as ${process.env.PORT || 5000}`)
+);
