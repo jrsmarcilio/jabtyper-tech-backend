c0f6f4d51b49bff567534a4e80a79a1b6d8394d2
jrsmarcilio <jrsmarcilio@gmail.com>
2021-11-19 01:24:54 -0300

    add heroku configs

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   "type": "postgres",
-  "URL": process.env.DATABASE_URL,
+  "URL": "postgres://liwhdibhtlrqvv:601628763f7c30da922e3a4e22e58e3f5cff6eca3fd92e3a6facea45af77a8ae@ec2-3-92-15-1.compute-1.amazonaws.com:5432/d7tk282k4c14f9",
   "ssl": true,
   "extra": {
     "ssl": { "rejectUnauthorized": false },
