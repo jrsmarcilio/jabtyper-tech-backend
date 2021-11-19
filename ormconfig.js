a411309fd827a74307c0c03740e49b0d5adede97
jrsmarcilio <jrsmarcilio@gmail.com>
2021-11-19 01:57:00 -0300

    add heroku configs

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   "type": "postgres",
-  "host": process.env.DB_HOST,
-  "port": process.env.DB_PORT,
-  "username": process.env.DB_USER,
-  "password": process.env.DB_PASSWORD,
-  "database": process.env.DB_NAME,
+  "host": "ec2-3-92-15-1.compute-1.amazonaws.com",
+  "port": 5432,
+  "username": "liwhdibhtlrqvv",
+  "password": "601628763f7c30da922e3a4e22e58e3f5cff6eca3fd92e3a6facea45af77a8ae",
+  "database": "d7tk282k4c14f9",
   "ssl": true,
   "extra": {
     "ssl": { "rejectUnauthorized": false },
